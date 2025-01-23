6212513bdb7b47089bb869a4e546abc06c300123
Administrator <mzdk100@foxmail.com>
2025-01-23 14:18:16 +0800

    更新版本至0.1.5并优化缓存目录创建逻辑

--- src/other.rs
+++ src/other.rs
@@ -1,8 +1,14 @@
-use std::path::PathBuf;
 use dirs::data_local_dir;
+use std::{fs::create_dir, path::PathBuf};
 
 pub(super) fn cache_dir() -> Option<PathBuf> {
-    data_local_dir().map(|p| p.join("Temp"))
+    data_local_dir().map(|p| p.join("Temp")).and_then(|p| {
+        if p.exists() {
+            Some(p)
+        } else {
+            create_dir(p.clone()).map(|_| p).ok()
+        }
+    })
 }
 
 pub(super) fn data_dir() -> Option<PathBuf> {
