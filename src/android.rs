19e1eeb29cc6a5b1c031db5f452aab1e98548c64
Administrator <mzdk100@foxmail.com>
2025-02-26 19:01:14 +0800

    更新版本至0.1.7并适配droid-wrap0.3.9。

--- src/android.rs
+++ src/android.rs
@@ -2,13 +2,17 @@ use droid_wrap::android::app::Activity;
 use std::path::PathBuf;
 
 pub(super) fn cache_dir() -> Option<PathBuf> {
-    Some(PathBuf::from(
-        &Activity::fetch().get_cache_dir().to_string(),
-    ))
+    let Ok(act) = Activity::fetch() else {
+        return None;
+    };
+
+    Some(PathBuf::from(&act.get_cache_dir().to_string()))
 }
 
 pub(super) fn data_dir() -> Option<PathBuf> {
-    Some(PathBuf::from(
-        &Activity::fetch().get_files_dir().to_string(),
-    ))
+    let Ok(act) = Activity::fetch() else {
+        return None;
+    };
+
+    Some(PathBuf::from(&act.get_files_dir().to_string()))
 }
